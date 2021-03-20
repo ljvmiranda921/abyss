61022aebc3b1dad5098a45f0de60896367befb25
Lj V. Miranda <ljvmiranda@gmail.com>
2021-03-20 10:33:29 +0800

    Add enemy attack animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -21,10 +21,21 @@ func move(dest_x, dest_y):
     tile_coord = Vector2(dest_x, dest_y)
 
 
-func attack(offset):
+func attack(anim_offset):
+    # Update animation
     sprite_anim.play("attack")
-    sprite_anim.set_offset(offset)
+    sprite_anim.set_offset(anim_offset)
     self.z_index = 100
+
+    # Apply actual damage to enemy
+
+
+func take_damage():
+    # Update animation
+    if sprite_anim.animation == "walk": 
+        # Only play damage when you're not attacking
+        # If you're attacking just combat anim
+        sprite_anim.play("take_damage")
 
 
 func _on_AnimatedSprite_animation_finished():
