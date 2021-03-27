7b58f9151603b3d2e3ea7621858f9f85751c7969
Lj V. Miranda <ljvmiranda@gmail.com>
2021-03-27 14:13:29 +0800

    [wip] Update

--- HUD.gd
+++ HUD.gd
@@ -8,8 +8,11 @@ func set_level(val):
     level.text =  "Level " + str(val+1)
 
 func set_hp(val):
-    hp.text = "HP " + str(val)
-    # TODO: modulate red if less than 30
+    hp.clear()
+    if val <= 50:
+        var fatal_color = Color("#e64e4b")
+        hp.push_color(fatal_color)
+    hp.add_text("HP " + str(val))
 
 func set_dmg(val):
     dmg.text = "Dmg " + str(val)
