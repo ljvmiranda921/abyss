740a5e9495212d5f0276a07a64f39bc172aa11bc
Lj V. Miranda <ljvmiranda@gmail.com>
2021-04-22 14:33:16 +0800

    [wip] Update

--- Enemies/CavernEnemies/EnemySkRogue.gd
+++ Enemies/CavernEnemies/EnemySkRogue.gd
@@ -3,7 +3,7 @@ extends AnimatedSprite
 onready var los_effect = $LOSEffect
 
 func _on_AnimatedSprite_animation_finished():
-    if self.animation != "default" && self.animation != "death":
+    if self.animation != "default" && self.animation != "death" && self.animation != "defend":
         self.play("default")
         self.set_offset(Vector2(0,0))
         self.z_index = 0
