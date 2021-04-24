762785e17a24f85b468add5c6ac7c90415abd9aa
Lj V. Miranda <ljvmiranda@gmail.com>
2021-04-25 02:15:35 +0800

    Create transition screen using fade rects

--- HUD.gd
+++ HUD.gd
@@ -4,6 +4,8 @@ onready var level = $InfoBox/Level
 onready var hp = $InfoBox/HP
 onready var dmg = $InfoBox/Damage
 onready var lose = $Screens/LoseScreen
+onready var transition = $Screens/SceneTransitionRect
+onready var transition_player = $Screens/SceneTransitionRect/AnimationPlayer
 
 signal restart_game
 
@@ -19,6 +21,12 @@ func set_hp(current, total):
 
 func set_dmg(val):
     dmg.text = str(val)
+
+func play_fade_in():
+    transition_player.play("Fade")
+    yield(transition_player, "animation_finished")
+    transition_player.play_backwards("Fade")
+    # yield(transition_player, "animation_finished")
 
 func _on_Button_pressed():
     emit_signal("restart_game")
