602cbab7a0de41bfec14a7a667e64922475ea845
Lj V. Miranda <ljvmiranda@gmail.com>
2021-05-10 00:09:18 +0800

    Stop all music when coming from death screen

--- StartScreen.gd
+++ StartScreen.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 func _ready():
     OS.set_window_size(Vector2(960, 540))
+    BackgroundMusic.stop_all()    
     BackgroundMusic.play(preload("res://music/start.ogg"))
 
 func _input(event):
